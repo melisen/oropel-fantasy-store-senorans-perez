a8a3bb48aa0a58a55a4abea826cd735b1c22ff79
Melina Señoráns Pérez <melina.senorans@gmail.com>
2022-06-24 19:23:27 -0300

    after

--- src/Components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/Components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,29 +1,32 @@
 //@ts-check
 import React, { useEffect, useState } from 'react'
 import ItemDetail from "./ItemDetail.jsx"
+import { useParams } from 'react-router-dom';
 
 export default function ItemDetailContainer() {
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(false);
     const [itemProducto, setItemProducto] = useState({});
 
-    let itemElegidoID=10;
+    //let {itemElegidoID}= useParams() ;
+    let itemElegidoID= 10;
 
     useEffect( ()=>{
         let promesaDetail = new Promise((resolve, rej) =>{
                 setTimeout( ()=>{
-                        resolve(
+                        
                             fetch("./listado-productos.json")
                             .then((response)=> response.json())
                             .then((data)=>{
-                                console.log(data);
+                                resolve(data);
+                                console.log("hola ",data);
                             })
-                        );
                 }, 2000)
             });
 
         promesaDetail
         .then((resultado)=> {
+            console.log(resultado)
                 let aux = resultado.find((elemento) => elemento.id == itemElegidoID)
                 setItemProducto(aux);
             console.log(itemProducto);
