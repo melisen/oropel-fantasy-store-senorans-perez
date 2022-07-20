213f7569abbd6e91c8e976775d626199027d362d
Melina Señoráns Pérez <melina.senorans@gmail.com>
2022-07-20 11:22:20 -0300

    cart resp

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
 
 
 export default function Footer() {
   return (
-    <div style={{position:'fixed', left:'0', bottom:'0', right:'0', display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center', backgroundColor:'#121810fa'}}>
+    <Container maxWidth="xl" style={{position:'fixed', left:'0', bottom:'0', right:'0', display:'flex', flex:'0 0 50px', marginTop:'auto', flexDirection:'column', justifyContent:'center', alignItems:'center', backgroundColor:'#121810fa'}}>
             <div style={{display:'flex', flexDirection:'row', justifyContent:'center', alignItems:'center'}}>
                 <Typography variant="h3" component="div" className="typographyBrand" sx={{ flexGrow: 1, color:'secondary', marginRight:'15px' }}>Oropel</Typography>
                 <Typography variant="h6" component="div" className="typographyBrand fantasyStore" sx={{ flexGrow: 1 }}>Fantasy Store</Typography>
             </div>
             <Typography variant="body1" component="div"  sx={{color:'primary.main', textAlign:'center', flexGrow: 1 }}>E-commerce desarrollado por Melina Señoráns Pérez para el React de Coderhouse julio 2022</Typography>
-    </div>
+    </Container>
   )
 }
